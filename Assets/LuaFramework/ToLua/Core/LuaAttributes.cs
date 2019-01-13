75640612ca25fb40200adbfb4979c2cdc44172d6
xiaogeformax <xiaogeformax@gmail.com>
2019-01-14 00:02:53 +0800

    LuaAttributes

---
+++ Assets/LuaFramework/ToLua/Core/LuaAttributes.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace LuaInterface
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    public sealed class MonoPInvokeCallbackAttribute : Attribute
+    {
+        public MonoPInvokeCallbackAttribute(Type type)
+        {
+        }
+    }
+
+    public class NoToLuaAttribute : System.Attribute
+    {
+        public NoToLuaAttribute()
+        {
+
+        }
+    }
+
+    public class UseDefinedAttribute : System.Attribute
+    {
+        public UseDefinedAttribute()
+        {
+
+        }
+    }
+
+    public sealed class LuaByteBufferAttribute : Attribute
+    {
+        public LuaByteBufferAttribute()
+        {
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Method)]
+    public sealed class LuaRenameAttribute : Attribute
+    {
+        public string Name;
+        public LuaRenameAttribute()
+        {
+        }
+    }
+}
