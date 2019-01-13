f36a5fc4cbbc9f8d379ee125eed408b3e1252005
xiaogeformax <xiaogeformax@gmail.com>
2019-01-14 00:00:16 +0800

    lua

---
+++ Assets/LuaFramework/ToLua/Core/LuaMisc.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using System.Runtime.CompilerServices;
+
+namespace LuaInterface
+{
+    public class GCRef
+    {
+        public int reference;
+        public string name = null;
+
+        public GCRef(int reference, string name)
+        {
+            this.reference = reference;
+            this.name = name;
+        }
+    }
+}
